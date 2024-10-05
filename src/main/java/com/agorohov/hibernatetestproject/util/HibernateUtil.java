014ef73654b8082585872e2167ff71bf56130cba
agorohov <aleksandr.gorohov@gmail.com>
2024-10-05 21:00:39 +0300

    добавил hibernate-platform

--- src/main/java/com/agorohov/hibernatetestproject/util/HibernateUtil.java
+++ src/main/java/com/agorohov/hibernatetestproject/util/HibernateUtil.java
@@ -7,64 +7,62 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-import java.util.Properties;
-
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
     // конфигурация через Java-код
-//    private static SessionFactory buildSessionFactory() {
-//        try {
-//            StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
-//
-//            registryBuilder.applySetting("hibernate.connection.driver_class", "org.postgresql.Driver");
-//            registryBuilder.applySetting("hibernate.connection.url", "jdbc:postgresql://localhost:5432/hibernate_test_project");
-//            registryBuilder.applySetting("hibernate.connection.username", "agorohov");
-//            registryBuilder.applySetting("hibernate.connection.password", "agorohov");
-//            registryBuilder.applySetting("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-//            registryBuilder.applySetting("hibernate.show_sql", "true");
-//            registryBuilder.applySetting("hibernate.hbm2ddl.auto", "update");
-//
-//            StandardServiceRegistry serviceRegistry = registryBuilder.build();
-//
-//            MetadataSources metadataSources = new MetadataSources(serviceRegistry);
-//            metadataSources.addAnnotatedClass(User.class);
-//
-//            Metadata metadata = metadataSources.getMetadataBuilder().build();
-//
-//            return metadata.getSessionFactoryBuilder().build();
-//        } catch (Throwable ex) {
-//            System.err.println("Initial SessionFactory creation failed" + ex);
-//            throw new ExceptionInInitializerError(ex);
-//        }
-//    }
-
-    // конфигурация через hibernate.properties
     private static SessionFactory buildSessionFactory() {
         try {
-            Properties properties = new Properties();
-            properties.load(HibernateUtil.class.getClassLoader().getResourceAsStream("hibernate.properties"));
-
             StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
-            registryBuilder.applySettings(properties);
 
-            StandardServiceRegistry standardRegistry = registryBuilder.build();
+            registryBuilder.applySetting("hibernate.connection.driver_class", "org.postgresql.Driver");
+            registryBuilder.applySetting("hibernate.connection.url", "jdbc:postgresql://localhost:5432/hibernate_test_project");
+            registryBuilder.applySetting("hibernate.connection.username", "agorohov");
+            registryBuilder.applySetting("hibernate.connection.password", "agorohov");
+            registryBuilder.applySetting("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+            registryBuilder.applySetting("hibernate.show_sql", "true");
+            registryBuilder.applySetting("hibernate.hbm2ddl.auto", "update");
 
-            // Создаем MetadataSources
-            MetadataSources metadataSources = new MetadataSources(standardRegistry);
-            metadataSources.addAnnotatedClass(User.class); // Добавляем сущность
+            StandardServiceRegistry serviceRegistry = registryBuilder.build();
 
-            // Создаем Metadata
+            MetadataSources metadataSources = new MetadataSources(serviceRegistry);
+            metadataSources.addAnnotatedClass(User.class);
+
             Metadata metadata = metadataSources.getMetadataBuilder().build();
 
-            // Создаем SessionFactory
             return metadata.getSessionFactoryBuilder().build();
         } catch (Throwable ex) {
             System.err.println("Initial SessionFactory creation failed" + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
+
+    // конфигурация через hibernate.properties
+//    private static SessionFactory buildSessionFactory() {
+//        try {
+//            Properties properties = new Properties();
+//            properties.load(HibernateUtil.class.getClassLoader().getResourceAsStream("hibernate.properties"));
+//
+//            StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
+//            registryBuilder.applySettings(properties);
+//
+//            StandardServiceRegistry standardRegistry = registryBuilder.build();
+//
+//            // Создаем MetadataSources
+//            MetadataSources metadataSources = new MetadataSources(standardRegistry);
+//            metadataSources.addAnnotatedClass(User.class); // Добавляем сущность
+//
+//            // Создаем Metadata
+//            Metadata metadata = metadataSources.getMetadataBuilder().build();
+//
+//            // Создаем SessionFactory
+//            return metadata.getSessionFactoryBuilder().build();
+//        } catch (Throwable ex) {
+//            System.err.println("Initial SessionFactory creation failed" + ex);
+//            throw new ExceptionInInitializerError(ex);
+//        }
+//    }
 
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
